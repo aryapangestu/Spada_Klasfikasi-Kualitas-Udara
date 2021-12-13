a9a56fb801b84d8087a6b74c93c621820f5007de
Arya Pangestu <61049998+aryapangestu@users.noreply.github.com>
2021-12-13 09:49:17 +0700

    Tugas besar SPADA

---
+++ app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import pickle
+
+MODEL_FILE = "modelKualitasUdaraRFC.pkl" #Random Forest Classifier dengan akurasi 99%
+pickle_in = open(MODEL_FILE, 'rb')
+classifier = pickle.load(pickle_in)
+
+st.set_page_config(
+    page_title="Klasifikasi Kualitas Udara - Arya Pangestu",
+    page_icon="7️⃣"
+)
+
+st.write("# Klasifikasi Kualitas Udara")
+st.markdown('By Arya Pangestu')
+
+st.write("Kualitas udara sekarang:")
+
+st.sidebar.markdown('## Atur Parameter Variabel')
+pm10 = st.sidebar.slider('pm10: Partikulat', 0, 150, 30) 
+pm25 = st.sidebar.slider('pm25: Partikulat', 0, 150, 48) 
+so2 = st.sidebar.slider('so2: Sulfida', 0, 150, 24)
+co = st.sidebar.slider('co: Carbon Monoksida', 0, 150, 4)
+o3 = st.sidebar.slider('o3: Ozon', 0, 150, 32)
+no2 = st.sidebar.slider('no2: Nitrogen dioksida', 0, 150, 7)
+
+prediction = classifier.predict([[pm10, pm25, so2, co, o3, no2]])
+if (prediction[0] == 0) :
+    st.success("Baik")
+elif (prediction[0] == 1) :
+    st.success("Sedang")
+elif (prediction[0] == 2) :
+    st.success("Tidak Sehat")
